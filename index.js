5e7b0a17ca81b21f76a6e9a0cd18cb78b29eb8ed
Kevin Sasse <sassek70@gmail.com>
2022-08-27 17:54:38 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = [];
+const event = "birthday";
+
+function writeCards(names, event) {
+       for (let i = 0; i < names.length; i++) {
+        names[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+       
+       };
+    return names;
+    };
+
+  function countDown(finalNumber){
+    while (finalNumber >= 0) {
+        console.log(finalNumber--);
+    }
+  }
