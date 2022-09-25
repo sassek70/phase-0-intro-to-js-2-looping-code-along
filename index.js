9d0d4428c7d13a948dd27745f8fa9f911f4ae9b0
Kevin Sasse <sassek70@gmail.com>
2022-09-25 16:02:47 -0400

    commit tree df667ff5a1fe60b552caa1edb4ff87bbd2714847

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const names = [];
-const event = "birthday";
+// const names = [];
+// const event = "birthday";
 
-function writeCards(names, event) {
-       for (let i = 0; i < names.length; i++) {
-        names[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+// function writeCards(names, event) {
+//        for (let i = 0; i < names.length; i++) {
+//         names[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
        
-       };
-    return names;
-    };
+//        };
+//     return names;
+//     };
 
-  function countDown(finalNumber){
-    while (finalNumber >= 0) {
-        console.log(finalNumber--);
+  // function countDown(finalNumber){
+  //   while (finalNumber >= 0) {
+  //       console.log(finalNumber--);
+  //   }
+  // }
+
+
+
+
+  function writeCards(names, party){
+    const surprise = [];
+    for (let i = 0; i < names.length; i++) {
+    const newString = (`Thank you, ${names[i]}, for the wonderful ${party} gift!`)
+    surprise.push(newString);
     }
-  }
+    return surprise;
+}
